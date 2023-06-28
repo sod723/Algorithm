fa1cf6f89f295a2565dce38c669b290e5d635dfe
송민석 <81648520+sod723@users.noreply.github.com>
2023-06-29 00:38:03 +0900

    Update 디스크 컨트롤러.py

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,3 +1,21 @@
+import heapq
+
 def solution(jobs):
-    answer = 0
-    return answer
+    count, last, answer = 0, -1, 0
+    heap = []
+    jobs.sort()
+    time = jobs[0][0]
+    
+    while count < len(jobs):
+        for s in jobs:
+            if last < s[0] <= time:
+                heapq.heappush(heap, (s[1], s[0]))
+        if len(heap) > 0:
+            current = heapq.heappop(heap)
+            last = time
+            time += current[0]
+            answer += time - current[1]
+            count += 1
+        else:
+            time += 1
+    return int(answer / len(jobs))
