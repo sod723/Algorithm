8b7151a4025ba75984318109a1c5cc631a0a5226
송민석 <81648520+sod723@users.noreply.github.com>
2023-10-01 00:26:15 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 117756 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+N, M = map(int,input().split())
+ans = []
+
+def backTracking(depth):
+    if depth == M:
+        print(" ".join(map(str, ans)))
+        return
+    for i in range(1, N+1):
+        if i in ans:
+            continue
+        ans.append(i)
+        backTracking(depth+1)
+        ans.pop()
+
+backTracking(0)
+
+
+
