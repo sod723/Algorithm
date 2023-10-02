94e494058a12fd9f9a69e3677a0e9d2825787a57
송민석 <81648520+sod723@users.noreply.github.com>
2023-10-03 01:17:28 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,11 +1,6 @@
 def solution(array, commands):
     answer = []
-    
-    for c in commands:
-        arr = []
-        for i in range(c[0] - 1, c[1]):
-            arr.append(array[i])
-        arr.sort()
-        answer.append(arr[c[2]-1])
-
+    for cmd in commands:
+        tmp = sorted(array[cmd[0]-1:cmd[1]])
+        answer.append(tmp[cmd[2]-1])
     return answer
