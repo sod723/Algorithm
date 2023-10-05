a3146845ee069e766aac987eeb141a7e6723e3db
송민석 <81648520+sod723@users.noreply.github.com>
2023-10-05 23:25:55 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,19 +1,17 @@
-def solution(k, dungeons):
-
-    answer = 0
-    visited = [False] * len(dungeons)
-    def dfs(k, cnt):
-        nonlocal answer
-        
-        if cnt > answer:
-            answer = cnt
-        
-        for i in range(len(dungeons)):
-            if not visited[i] and k >= dungeons[i][0]:
-                visited[i] = True
-                dfs(k-dungeons[i][1], cnt+1)
-                visited[i] = False
-        
+answer = 0
+visited = []
+def dfs(k, dungeons, cnt):
+    global answer, visited
+    if cnt > answer:
+        answer = cnt
+    for i in range(len(dungeons)):
+        if k >= dungeons[i][0] and not visited[i]:
+            visited[i] = True
+            dfs(k-dungeons[i][1], dungeons, cnt + 1)
+            visited[i] = False
     
-    dfs(k, 0)
+def solution(k, dungeons):
+    global answer, visited
+    visited = [False for _ in range(len(dungeons))]
+    dfs(k, dungeons, 0)
     return answer
